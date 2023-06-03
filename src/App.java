753c6d4620ea94122d8b9023ca66eff0b194518a
203-EĞİTMEN PC <mksular@gmail.com>
2023-06-03 18:32:52 +0300

    right panel ve menu bar tamamlandı

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        TaskList todoList = new TaskList();
+        todoList.setSize(480, 400);
+        todoList.setVisible(true);
     }
 }
---
+++ src/Category.java
@@ -0,0 +1,16 @@
+
+public class Category {
+
+    int id;
+    String title;
+
+    public Category(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+}
